bc9e3d1487941d05b7f32c0789a3b64ac706b711
savairadad <savairadadmohammad@gmail.com>
2022-09-28 20:11:23 -0400

    first commit

---
+++ src/class04/FinalClass.java
@@ -0,0 +1,18 @@
+package class04;
+
+public class FinalClass {
+
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println("this is the code for the mercedes");
+		
+		
+		
+		
+		
+		
+	}
+
+}
